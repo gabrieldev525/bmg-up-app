bb6010c79405494cadb515520a78af86e5a15873
Victor Lourenco <vlourenco.m@gmail.com>
2020-05-30 20:54:58 -0300

    Added header component

---
+++ frontend/src/components/header/styles.js
@@ -0,0 +1,11 @@
+import { StyleSheet } from 'react-native';
+
+export default StyleSheet.create({
+    header: {
+        height: 80,
+        width: "100%",
+        backgroundColor: '#fe9a2e',
+        justifyContent: "center",
+        paddingHorizontal: 10, 
+    },
+});

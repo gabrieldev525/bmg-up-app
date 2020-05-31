668621c42de4e996503d9b173263d91f15cc87cb
Victor Lourenco <vlourenco.m@gmail.com>
2020-05-31 20:42:34 -0300

    Update page 1 from quiz

--- frontend/src/pages/quiz/Page_1/index.js
+++ frontend/src/pages/quiz/Page_1/index.js
@@ -4,6 +4,8 @@ import { useNavigation } from '@react-navigation/native';
 import AppHeader from '../../../components/header';
 import styles from './styles';
 
+import RadioOption from '../../../components/radioOption';
+
 export default function QuizPage1() {
     const navigation = useNavigation();
 
@@ -15,6 +17,20 @@ export default function QuizPage1() {
             <AppHeader />
             <View style={styles.container}>
                 <Text style={styles.title}>Vamos descobrir qual o seu conhecimento sobre investimento?</Text>
+                <View style={styles.radioButton}>
+                    <RadioOption 
+                        text="Iniciante: Não sabe sore nenhum tipo de investimento e/ou já deixou seu dinheiro em poupança."
+                        textStyle={{paddingLeft: 10}}
+                    />
+                    <RadioOption 
+                        text="Intermediário: Tem conhecimento sobre CDI e CDB e investimentos mais sofisticados."
+                        textStyle={{paddingLeft: 10}}
+                    />
+                    <RadioOption 
+                        text="Avançado: Já tem um alto grau de conhecimento de investimentos, como a bolsa de valores e outros."
+                        textStyle={{paddingLeft: 10}}
+                    />
+                </View>
                 <Text style={styles.paragraph}>Analisando o seu perfil, vamos te apresentar pequenos exemplos com algumas definições. 
                     Tudo de forma intuitiva.
                 </Text>

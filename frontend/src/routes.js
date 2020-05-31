d5c3d67501414d71592fe6298e720c65d02dcbf4
Victor Lourenco <vlourenco.m@gmail.com>
2020-05-31 08:33:14 -0300

    Page storeValue and rescueValue

--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -4,6 +4,8 @@ import { createStackNavigator } from '@react-navigation/stack';
 
 import Initial from './pages/initial';
 import List from './pages/list';
+import StoreValue from './pages/storeValue'
+import RescueValue from './pages/rescueValue'
 
 const AppStack = createStackNavigator();
 
@@ -11,6 +13,8 @@ export default function Routes() {
     return (
         <NavigationContainer>
             <AppStack.Navigator screenOptions={{ headerShown: false }}>
+                <AppStack.Screen name="StoreValue" component={StoreValue} />
+                <AppStack.Screen name="RescueValue" component={RescueValue} />
                 <AppStack.Screen name="Initial" component={Initial} />
                 <AppStack.Screen name="List" component={List} />
             </AppStack.Navigator>
